40e3300fd39cdba1272f467cc83d08dac6c06b75
wanghai <wang6hai@gmail.com>
2018-11-01 18:47:24 +0800

    delete method box_rotate, move a few methods to angle.py and text_detect.py.

--- core.py
+++ core.py
@@ -2,56 +2,9 @@ import config as cfg
 import numpy as np
 from PIL import Image
 from numpy import cos, sin
-from detector.detectors import TextDetector
-from detector.other import get_boxes
 from crnn.crnn import crnnOcr as crnnOcr
 from core_helper.angle import eval_angle
-
-if cfg.opencvFlag:
-    import opencv_dnn_detect as detect
-else:
-    import darknet_detect as detect
-
-
-def text_detect(img,
-                MAX_HORIZONTAL_GAP=30,
-                MIN_V_OVERLAPS=0.6,
-                MIN_SIZE_SIM=0.6,
-                TEXT_PROPOSALS_MIN_SCORE=0.7,
-                TEXT_PROPOSALS_NMS_THRESH=0.3,
-                TEXT_LINE_NMS_THRESH=0.3,
-                MIN_RATIO=1.0,
-                LINE_MIN_SCORE=0.8,
-                TEXT_PROPOSALS_WIDTH=5,
-                MIN_NUM_PROPOSALS=1,):
-    boxes, scores = detect.text_detect(np.array(img))
-
-    boxes = np.array(boxes, dtype=np.float32)
-    scores = np.array(scores, dtype=np.float32)
-    textdetector = TextDetector(MAX_HORIZONTAL_GAP, MIN_V_OVERLAPS, MIN_SIZE_SIM)
-    shape = img.size[::-1]
-    boxes = textdetector.detect(boxes,
-                                scores[:, np.newaxis],
-                                shape,
-                                TEXT_PROPOSALS_MIN_SCORE,
-                                TEXT_PROPOSALS_NMS_THRESH,
-                                TEXT_LINE_NMS_THRESH,
-                                MIN_RATIO,
-                                LINE_MIN_SCORE,
-                                TEXT_PROPOSALS_WIDTH,
-                                MIN_NUM_PROPOSALS)
-
-    text_recs, tmp = get_boxes(np.array(img), boxes)
-    newBox = []
-    rx = 1
-    ry = 1
-    for box in text_recs:
-        x1, y1 = (box[0], box[1])
-        x2, y2 = (box[2], box[3])
-        x3, y3 = (box[6], box[7])
-        x4, y4 = (box[4], box[5])
-        newBox.append([x1 * rx, y1 * ry, x2 * rx, y2 * ry, x3 * rx, y3 * ry, x4 * rx, y4 * ry])
-    return newBox, tmp
+from core_helper.text_detect import text_detect
 
 
 def crnnRec(im, text_recs, if_im=False, left_adjust=False, right_adjust=False, alph=0.2):
@@ -72,40 +25,12 @@ def crnnRec(im, text_recs, if_im=False, left_adjust=False, right_adjust=False, a
         partImg, newW, newH = rotate_cut_img(im, degree, rec, w, h, left_adjust, right_adjust, alph)
         newBox = xy_rotate_box(cx, cy, newW, newH, degree)
         partImg_ = partImg.convert('L')
-        simPred = crnnOcr(partImg_)  ##识别的文本
+        simPred = crnnOcr(partImg_)  # 识别的文本
         if simPred.strip() != u'':
             results.append(
                 {'cx': cx, 'cy': cy, 'text': simPred, 'w': newW, 'h': newH, 'degree': degree * 180.0 / np.pi})
 
     return results
-
-
-def box_rotate(rec, angle=0, imgH=0, imgW=0):
-    """
-    对坐标进行旋转 逆时针方向 0\90\180\270,
-    """
-    x1, y1, x2, y2, x3, y3, x4, y4 = rec[:8]
-    if angle == 90:
-        x1_, y1_ = y2, imgW - x2
-        x2_, y2_ = y3, imgW - x3
-        x3_, y3_ = y4, imgW - x4
-        x4_, y4_ = y1, imgW - x1
-
-    elif angle == 180:
-        x1_, y1_ = imgW - x3, imgH - y3
-        x2_, y2_ = imgW - x4, imgH - y4
-        x3_, y3_ = imgW - x1, imgH - y1
-        x4_, y4_ = imgW - x2, imgH - y2
-
-    elif angle == 270:
-        x1_, y1_ = imgH - y4, x4
-        x2_, y2_ = imgH - y1, x1
-        x3_, y3_ = imgH - y2, x2
-        x4_, y4_ = imgH - y3, x3
-    else:
-        x1_, y1_, x2_, y2_, x3_, y3_, x4_, y4_ = x1, y1, x2, y2, x3, y3, x4, y4
-
-    return (x1_, y1_, x2_, y2_, x3_, y3_, x4_, y4_)
 
 
 def solve(box):
@@ -221,15 +146,17 @@ def model(img, detect_angle=False, config={}, if_im=True, left_adjust=False, rig
     :param if_adjust_degree:
     :return:
     """
+    # 1. 角度检测、修正等
     angle, degree, img = eval_angle(img, detectAngle=detect_angle, ifadjustDegree=if_adjust_degree)
-
     img = letterbox_image(img, cfg.IMGSIZE)
 
+    # 2. 画文本框
     config['img'] = img
     text_recs, tmp = text_detect(**config)
+    sorted_box = sort_box(text_recs)
 
-    newBox = sort_box(text_recs)
-    result = crnnRec(np.array(img), newBox, if_im, left_adjust, right_adjust, alph)
+    # 3. 识别文本
+    result = crnnRec(np.array(img), sorted_box, if_im, left_adjust, right_adjust, alph)
     return img, result, angle
 
 

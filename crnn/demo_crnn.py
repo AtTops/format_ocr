2803a63f47d408ce9685a705e6f91528e490bdcd
wanghai <wang6hai@gmail.com>
2018-11-08 13:00:49 +0800

    避免太细长放入网络出错

--- crnn/demo_crnn.py
+++ crnn/demo_crnn.py
@@ -9,20 +9,28 @@ from PIL import Image
 from crnn.crnn_ import crnnOcr as crnnOcr
 import csv
 
-paths = glob('../testA_part/all/*.jpg')
+paths = glob('../testA_part/trn/*.jpg')
 # paths = glob('./img/b.jpg')
 
 if __name__ == '__main__':
-    with open('testA_part_result_only_crnn.csv', 'w') as csv_file:
+    with open('train_part_result_only_crnn.csv', 'w') as csv_file:
         fieldheader = ['filename', 'content']
         writer = csv.DictWriter(csv_file, fieldheader)
         writer.writeheader()
+        count = 0
         for path in paths:
-            print(path[18:-4])
+            # print(path[18:-4])
             img = Image.open(path).convert('L')
+            # print('size_first', img.size)
+            width, height = img.size
+            # 避免太细长放入网络出错
+            if height / width > 7:
+                img = img.resize((int(height / 7) + 1, height), Image.BILINEAR)
+                count += 1
+                img.show()
             simPred = crnnOcr(img)
             print(simPred)
             row = {}  # TODO 修改为多行一次写入
             row.update(filename=path[18:-4], content=simPred)
             writer.writerow(row)
-
+        print('done! ', count)
